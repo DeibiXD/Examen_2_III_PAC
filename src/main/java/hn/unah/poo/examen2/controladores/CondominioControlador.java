8968eed22a93ee6449d7e0ef78ca9ec3c0712150
DeibiXD <dparad456@gmail.com>
2024-11-21 19:40:24 -0600

    cambios hasta paso 3

--- src/main/java/hn/unah/poo/examen2/controladores/CondominioControlador.java
+++ src/main/java/hn/unah/poo/examen2/controladores/CondominioControlador.java
@@ -3,8 +3,25 @@ package hn.unah.poo.examen2.controladores;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import hn.unah.poo.examen2.dtos.CondominioDTO;
+import hn.unah.poo.examen2.servicios.CondominioServicio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 @RequestMapping("/api/condominio")
 public class CondominioControlador {
+    
+     @Autowired
+    private CondominioServicio condominioServicio;
+
+    @PostMapping("/crear")
+    public String crearCondominio(@RequestBody CondominioDTO condominioDTO) {
+        
+        return condominioServicio.crearCondominio(condominioDTO);
+    }
     
 }
--- src/main/java/hn/unah/poo/examen2/servicios/CondominioServicio.java
+++ src/main/java/hn/unah/poo/examen2/servicios/CondominioServicio.java
@@ -1,8 +1,15 @@
 package hn.unah.poo.examen2.servicios;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import hn.unah.poo.examen2.dtos.CondominioDTO;
+import hn.unah.poo.examen2.dtos.MensaulidadesDTO;
 import hn.unah.poo.examen2.repositorios.CondominioRepositiorio;
 
 @Service
@@ -10,4 +17,20 @@ public class CondominioServicio {
     
     @Autowired
     private CondominioRepositiorio condominioRepositiorio;
+
+    public String crearCondominio(CondominioDTO condominioDTO) {
+        LocalDate fechaHoy = LocalDate.now();
+        if (!condominioRepositiorio.existsById(condominioDTO.getNumeroCondominio())){
+            double monto,area;
+            area = condominioDTO.getArea().doubleValue();
+            monto = area *2.50;
+
+            List<MensaulidadesDTO> mensaulidadesDTOs = new ArrayList<>();
+            
+            return "Se agrego correctamente el condominio";
+        }
+
+
+        return "Este condominio ya existe";
+    }
 }
